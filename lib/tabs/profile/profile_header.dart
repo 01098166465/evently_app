1c3d15bf936014d3c9d2e589c2daaa259152b53d
Mahmoud Khaled <christogram105@gmail.com>
2025-07-22 18:15:33 +0300

    implement profile tab ui

---
+++ lib/tabs/profile/profile_header.dart
@@ -0,0 +1,49 @@
+import 'package:evently/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class ProfileHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+    Size screenSize = MediaQuery.sizeOf(context);
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: AppTheme.primary,
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(64)),
+      ),
+
+      child: SafeArea(
+        child: Row(
+          children: [
+            Image.asset(
+              "assets/images/route_logo.png",
+              height: screenSize.height * 0.12,
+              fit: BoxFit.fill,
+            ),
+            SizedBox(width: 16),
+
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+
+                children: [
+                  Text("Mahmoud khaled", style: textTheme.headlineSmall),
+                  SizedBox(height: 10),
+                  Text(
+                    "mahmpoudkhaled@gmail.com",
+                    style: textTheme.titleMedium!.copyWith(
+                      color: AppTheme.white,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/tabs/profile/profile_tab.dart
+++ lib/tabs/profile/profile_tab.dart
@@ -1,8 +1,90 @@
+import 'package:evently/app_theme.dart';
+import 'package:evently/tabs/profile/profile_header.dart';
 import 'package:flutter/material.dart';
 
 class ProfileTab extends StatelessWidget {
+  List<Language> language = [
+    Language(code: "en", name: "English"),
+    Language(code: "ar", name: "العربية"),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("profile"));
+    TextTheme textTheme = Theme.of(context).textTheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ProfileHeader(),
+        SizedBox(height: 24),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Language",
+                    style: textTheme.titleLarge!.copyWith(
+                      color: AppTheme.black,
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: AppTheme.primary),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: DropdownButton(
+                      borderRadius: BorderRadius.circular(16),
+                      underline: SizedBox(),
+                      iconEnabledColor: AppTheme.primary,
+
+                      value: "en",
+                      style: textTheme.titleLarge!.copyWith(
+                        color: AppTheme.primary,
+                      ),
+                      items: language
+                          .map(
+                            (language) => DropdownMenuItem(
+                              value: language.code,
+                              child: Text(language.name),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Dark Theme",
+                    style: textTheme.titleLarge!.copyWith(
+                      color: AppTheme.black,
+                    ),
+                  ),
+                  Switch(
+                    value: true,
+                    activeColor: AppTheme.primary,
+                    onChanged: (value) {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
+}
+
+class Language {
+  String code;
+  String name;
+  Language({required this.code, required this.name});
 }
